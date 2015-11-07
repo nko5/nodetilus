3c1c375142db50666c38696178913ec87989b497
Jaime Garcia <jgarciaqo@gmail.com>
2015-11-07 13:24:51 -0600

    draft

---
+++ eg-github_response_search.js
@@ -0,0 +1,156 @@
+{
+    id: 943149,
+    name: 'd3',
+    full_name: 'mbostock/d3',
+    owner: {
+        login: 'mbostock',
+        id: 230541,
+        avatar_url: 'https://avatars.githubusercontent.com/u/230541?v=3',
+        gravatar_id: '',
+        url: 'https://api.github.com/users/mbostock',
+        html_url: 'https://github.com/mbostock',
+        followers_url: 'https://api.github.com/users/mbostock/followers',
+        following_url: 'https://api.github.com/users/mbostock/following{/other_user}',
+        gists_url: 'https://api.github.com/users/mbostock/gists{/gist_id}',
+        starred_url: 'https://api.github.com/users/mbostock/starred{/owner}{/repo}',
+        subscriptions_url: 'https://api.github.com/users/mbostock/subscriptions',
+        organizations_url: 'https://api.github.com/users/mbostock/orgs',
+        repos_url: 'https://api.github.com/users/mbostock/repos',
+        events_url: 'https://api.github.com/users/mbostock/events{/privacy}',
+        received_events_url: 'https://api.github.com/users/mbostock/received_events',
+        type: 'User',
+        site_admin: false
+    },
+    private: false,
+    html_url: 'https://github.com/mbostock/d3',
+    description: 'A JavaScript visualization library for HTML and SVG.',
+    fork: false,
+    url: 'https://api.github.com/repos/mbostock/d3',
+    forks_url: 'https://api.github.com/repos/mbostock/d3/forks',
+    keys_url: 'https://api.github.com/repos/mbostock/d3/keys{/key_id}',
+    collaborators_url: 'https://api.github.com/repos/mbostock/d3/collaborators{/collaborator}',
+    teams_url: 'https://api.github.com/repos/mbostock/d3/teams',
+    hooks_url: 'https://api.github.com/repos/mbostock/d3/hooks',
+    issue_events_url: 'https://api.github.com/repos/mbostock/d3/issues/events{/number}',
+    events_url: 'https://api.github.com/repos/mbostock/d3/events',
+    assignees_url: 'https://api.github.com/repos/mbostock/d3/assignees{/user}',
+    branches_url: 'https://api.github.com/repos/mbostock/d3/branches{/branch}',
+    tags_url: 'https://api.github.com/repos/mbostock/d3/tags',
+    blobs_url: 'https://api.github.com/repos/mbostock/d3/git/blobs{/sha}',
+    git_tags_url: 'https://api.github.com/repos/mbostock/d3/git/tags{/sha}',
+    git_refs_url: 'https://api.github.com/repos/mbostock/d3/git/refs{/sha}',
+    trees_url: 'https://api.github.com/repos/mbostock/d3/git/trees{/sha}',
+    statuses_url: 'https://api.github.com/repos/mbostock/d3/statuses/{sha}',
+    languages_url: 'https://api.github.com/repos/mbostock/d3/languages',
+    stargazers_url: 'https://api.github.com/repos/mbostock/d3/stargazers',
+    contributors_url: 'https://api.github.com/repos/mbostock/d3/contributors',
+    subscribers_url: 'https://api.github.com/repos/mbostock/d3/subscribers',
+    subscription_url: 'https://api.github.com/repos/mbostock/d3/subscription',
+    commits_url: 'https://api.github.com/repos/mbostock/d3/commits{/sha}',
+    git_commits_url: 'https://api.github.com/repos/mbostock/d3/git/commits{/sha}',
+    comments_url: 'https://api.github.com/repos/mbostock/d3/comments{/number}',
+    issue_comment_url: 'https://api.github.com/repos/mbostock/d3/issues/comments{/number}',
+    contents_url: 'https://api.github.com/repos/mbostock/d3/contents/{+path}',
+    compare_url: 'https://api.github.com/repos/mbostock/d3/compare/{base}...{head}',
+    merges_url: 'https://api.github.com/repos/mbostock/d3/merges',
+    archive_url: 'https://api.github.com/repos/mbostock/d3/{archive_format}{/ref}',
+    downloads_url: 'https://api.github.com/repos/mbostock/d3/downloads',
+    issues_url: 'https://api.github.com/repos/mbostock/d3/issues{/number}',
+    pulls_url: 'https://api.github.com/repos/mbostock/d3/pulls{/number}',
+    milestones_url: 'https://api.github.com/repos/mbostock/d3/milestones{/number}',
+    notifications_url: 'https://api.github.com/repos/mbostock/d3/notifications{?since,all,participating}',
+    labels_url: 'https://api.github.com/repos/mbostock/d3/labels{/name}',
+    releases_url: 'https://api.github.com/repos/mbostock/d3/releases{/id}',
+    created_at: '2010-09-27T17:22:42Z',
+    updated_at: '2015-11-07T16:46:43Z',
+    pushed_at: '2015-11-06T18:13:44Z',
+    git_url: 'git://github.com/mbostock/d3.git',
+    ssh_url: 'git@github.com:mbostock/d3.git',
+    clone_url: 'https://github.com/mbostock/d3.git',
+    svn_url: 'https://github.com/mbostock/d3',
+    homepage: 'https://d3js.org',
+    size: 109945,
+    stargazers_count: 43185,
+    watchers_count: 43185,
+    language: 'JavaScript',
+    has_issues: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: true,
+    forks_count: 11422,
+    mirror_url: null,
+    open_issues_count: 253,
+    forks: 11422,
+    open_issues: 253,
+    watchers: 43185,
+    default_branch: 'master',
+    score: 11.488287
+}
+
+
+url_package https://raw.githubusercontent.com/mbostock/d3/master/package.json
+
+{
+  "name": "d3",
+  "version": "3.5.6",
+  "description": "A JavaScript visualization library for HTML and SVG.",
+  "keywords": [
+    "dom",
+    "w3c",
+    "visualization",
+    "svg",
+    "animation",
+    "canvas"
+  ],
+  "homepage": "http://d3js.org",
+  "author": {
+    "name": "Mike Bostock",
+    "url": "http://bost.ocks.org/mike"
+  },
+  "contributors": [
+    {
+      "name": "Jason Davies",
+      "url": "http://jasondavies.com"
+    }
+  ],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/mbostock/d3.git"
+  },
+  "main": "index.js",
+  "browserify": "d3.js",
+  "jspm": {
+    "main": "d3",
+    "shim": {
+      "d3": {
+        "exports": "d3"
+      }
+    },
+    "files": [
+      "d3.js"
+    ],
+    "buildConfig": {
+      "uglify": true
+    }
+  },
+  "jam": {
+    "main": "d3.js",
+    "shim": {
+      "exports": "d3"
+    }
+  },
+  "spm": {
+    "main": "d3.js"
+  },
+  "devDependencies": {
+    "jsdom": "3",
+    "seedrandom": "2",
+    "smash": "0.0",
+    "uglify-js": "2.4.16",
+    "vows": "0.8"
+  },
+  "scripts": {
+    "test": "vows; echo"
+  },
+  "license": "BSD-3-Clause"
+}
