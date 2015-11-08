54d3db58856b4a2b85bdedafc362138cac000f10
Octavio Fuentes Arce <tavofuentes@gmail.com>
2015-11-07 20:34:09 -0600

    Added loading overlay.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
   init();
 });
 
+
+
 function init() {
   console.log('init');
+
+  $("#nautilusForm" ).submit(function( event ) {
+  	enableLoading();
+  	event.preventDefault();
+  });
+}
+
+
+
+function enableLoading() {
+	$("#nautilusForm :input").prop("disabled", true);
+	$('#loading-overlay').show();
+}
+
+function disbleLoading() {
+	$('#loading-overlay').hide();
+	$("#nautilusForm :input").prop("disabled", false);
 }
