78e985a5fa4a1d31785ea368ca7154131ca4d8a4
Octavio Fuentes Arce <tavofuentes@gmail.com>
2015-11-07 21:41:43 -0600

    Fixed code style.

--- public/js/main.js
+++ public/js/main.js
@@ -3,37 +3,35 @@ $(document).ready(function(){
 });
 
 function init() {
-  console.log('init');
-
   $('#nautilus_form' ).submit(getRepos);
 }
 
 function getRepos( event ) {
-	var data = { repo_url: $('#repo_url').val()};
+  var data = { repo_url: $('#repo_url').val()};
 
-	enableLoading();
+  enableLoading();
 
-	$.get( '/api/repo', data, function(response) {
-		console.log('Search result:', response);
-		displayResults(response);
-	});
+  $.get( '/api/repo', data, function(response) {
+    console.log('Search result:', response);
+    displayResults(response);
+  });
 
-	event.preventDefault();
+  event.preventDefault();
 }
 
 function displayResults(response) {
-	setTimeout(function() {
-		disbleLoading();
-		$('#result').text(response);
-	}, 300);
+  setTimeout(function() {
+    disbleLoading();
+    $('#result').text(response);
+  }, 300);
 }
 
 function enableLoading() {
-	$('#nautilusForm :input').prop('disabled', true);
-	$('#loading-overlay').show();
+  $('#nautilusForm :input').prop('disabled', true);
+  $('#loading-overlay').show();
 }
 
 function disbleLoading() {
-	$('#nautilusForm :input').prop('disabled', false);
-	$('#loading-overlay').hide();
+  $('#nautilusForm :input').prop('disabled', false);
+  $('#loading-overlay').hide();
 }
